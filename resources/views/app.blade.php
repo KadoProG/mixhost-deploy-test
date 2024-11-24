ae59ef38cd21e86ca7aa6bea7efde3e4190acb3f
KadoProG <splatrain@gmail.com>
2024-11-25 08:22:53 +0900

    init commit

---
+++ resources/views/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>これはタイトル</title>
+    <script src="/build/assets/app-CuLJPCj_.js" defer></script>
+</head>
+
+<body>
+    <div id="root"></div>
+</body>
+
+</html>
